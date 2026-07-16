d6af5de590febb7131689771a6c67d768ff06e7f
Abisola Fagbemi <h.fagbemi@yahoo.co.uk>
2026-07-16 06:52:23 +0100

    Update hero spacing.

--- components/views/Landing/LandingPage.tsx
+++ components/views/Landing/LandingPage.tsx
@@ -930,7 +930,7 @@ export default function LandingPage() {
 
           <div className="mx-auto max-w-7xl px-6 pt-10 pb-24 sm:pb-32 lg:flex lg:px-8 lg:py-40">
             <div className="mx-auto max-w-2xl shrink-0 lg:mx-0 lg:pt-8">
-              <div className="mt-24 sm:mt-32 lg:mt-16">
+              <div className="mt-2 sm:mt-32 lg:mt-16">
                 <a
                   href="#features"
                   className="inline-flex flex-wrap items-center gap-x-3 gap-y-2"
